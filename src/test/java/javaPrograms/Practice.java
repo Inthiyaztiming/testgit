03313655e6ca97f953a2319992ca0c71cebbbe36
Inthiyaztiming <inthiyaz.m@timingindia.com>
2026-02-02 10:56:51 +0530

    practice.java is modified

--- src/test/java/javaPrograms/Practice.java
+++ src/test/java/javaPrograms/Practice.java
@@ -1,15 +1,10 @@
 package javaPrograms;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+public class Practice 
+{
 
-public class Practice {
-	public static void main(String[] args) throws IOException 
-	{	
-			Integer arr[]= {1,2,3};
-			List<Integer>li= Arrays.asList(arr);
-			Collections.max(li);
-		
-	}}
+	
+	
+	}
+	
+
